81083c5edb89b93d692e54964fb7e39efc87e450
andrewkadry <andrew.raghib@gmail.com>
2018-08-14 12:46:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     FB_PAGE_TOKEN: '',
     FB_VERIFY_TOKEN: 'alongstringforSayBye',
     API_AI_CLIENT_ACCESS_TOKEN: '236b815bf09643cca6ba3de4ce47c2a7',
-    FB_APP_SECRET: '702d22a84c7ee06fa3db151dccd3b043',
+    FB_APP_SECRET: '',
     SERVER_URL: "https://saybye.herokuapp.com/",
     PAGEID: '',
     appID: '290935834787631'
